a0da4357cbd58a7a23ff6dfa19d04da4a936fe81
Gary Frederick <imthaghost@protonmail.com>
2022-05-01 15:03:00 -0700

    update

--- cmd/bookrating/main.go
+++ cmd/bookrating/main.go
@@ -1,48 +1,10 @@
+
 package main
 
-import (
-	"context"
-	"github.com/imthaghost/bookrating/config"
-	"github.com/imthaghost/bookrating/internal/notes/notion"
-	"github.com/imthaghost/bookrating/internal/reviews"
-	"github.com/k0kubun/pp"
-	"log"
-)
+import "github.com/imthaghost/bookrating/cmd"
 
+
+// Execute :)
 func main() {
-	csvPath := "data/ratings.csv"
-	// config service
-	configService := config.New{}
-	configService.Load()
-	cfg := configService.Get()
-
-	// database ID
-	databaseID := cfg.Notion.DatabaseID
-
-	// init Notion client
-	notionClient := notion.New(cfg)
-
-	// create book reviews
-	reviewedBooks, err := reviews.CreateBookClubReview(csvPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pp.Println("Loading books in Notion...")
-	// cross-check with Notion to see if a review already exists
-	for _, bookReview := range reviewedBooks {
-		// if the book already exists we don't really have to do anything
-		_, exist := notionClient.BookExists(context.Background(), bookReview.Title, databaseID)
-		if exist {
-			// nothing
-		} else {
-			// insert
-			var favorites float64
-			favorites = float64(bookReview.Favorites)
-			err := notionClient.InsertReview(context.Background(), databaseID, bookReview.Title, &bookReview.AverageRating, &favorites)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	cmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -9,8 +9,11 @@ require (
 )
 
 require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
